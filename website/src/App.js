f9ebb916583431384f3f0a77334cedf5bdd09fd8
rnv19 <routulasundeep@msitprogram.net>
2020-06-21 12:03:40 +0530

    Update App.js

--- website/src/App.js
+++ website/src/App.js
@@ -1,16 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Homepage from './Homepage'
+import Requestaquote from './Requestaquote'
+import { Nav, Navbar } from 'react-bootstrap'
+import Blog from './Blog'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Homepage />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      activeKey: "3"
+    }
+  }
+
+  handleClick = (selectedKey) => {
+    this.setState({ activeKey: selectedKey })
+  }
+
+  render() {
+    console.log(this.state.activeKey)
+    // if (this.state.activeKey === "3") {
+    //   return <Blog />
+    // } else return null
+    return (
+      <div className="App" >
+        <div>
+          <Navbar fixed="top" activeKey={this.state.activeKey} bg="dark" variant="dark">
+            <Navbar.Brand href="#home">FindMeALead</Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link eventKey={1} active={this.state.activeKey} onSelect={this.handleClick}>Home</Nav.Link>
+              <Nav.Link eventKey={2} active={this.state.activeKey} onSelect={this.handleClick}>Request A Quote</Nav.Link>
+              <Nav.Link eventKey={3} active={this.state.activeKey} onSelect={this.handleClick}>Blog</Nav.Link>
+            </Nav>
+          </Navbar>
+        </div>
+        {this.state.activeKey === "1" ? <Homepage /> : null}
+        {this.state.activeKey === "2" ? <Requestaquote /> : null}
+        {this.state.activeKey === "3" ? <Blog /> : null}
+      </div >
+    );
+  }
 }
 
 export default App;
